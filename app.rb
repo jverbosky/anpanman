c624716d5661e8ccfff2884f82e2712300874af3
jverbosk <jverbosk@gmail.com>
2017-02-14 14:40:28 -0500

    fixed mobile view via meta/viewport line in layout.erb, tweaked content and comments

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@
 ###### Anpanman Tribute Page ######
 ###################################
 # Use to test Bootstrap on mobile #
-# - Working via CodePen           #
+# - Working on CodePen            #
 ###################################
 
 # Note: jQuery buttons in hiragana table are not working
@@ -10,6 +10,10 @@
 
 require 'sinatra'
 require 'sinatra/reloader' if development?  # automatically reload app.rb on save via sinatra-contrib gem
+
+# set :bind, '0.0.0.0'  # allow other computers on network to access via http://ipaddress/4567
+# better to use the -o option when running via terminal:
+#   ruby app.rb -o 0.0.0.0
 
 get '/' do  # route to load the main page
   erb :main  # load main.erb file (mainly a placeholder, populated via layout.erb)
